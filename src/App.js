984465d87e4caf271dfc8fdc5d31868105877d92
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-07-22 14:46:03 +0200

    fetch weather data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { base_url, api_key } from "./components/Configs/Api";
+import "./App.css";
+import Weather from "./components/Weather/Weather";
 
 function App() {
+  const [weatherData, setWeatherData] = useState([]);
+  useEffect(() => {
+    const getWeather = async () => {
+      const response = await axios.get(
+        `${base_url}?lat=49.183333&lon=-0.350000&appid=${api_key}&lang=fr`
+      );
+      setWeatherData(response.data);
+    };
+    getWeather();
+  }, []);
+
+  //console.log(weatherData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <p>WEATHER_APP</p>
+        <div className="search">
+          <input placeholder="search location" type="text" />
+        </div>
+      </div>
+      <Weather data={weatherData} />
     </div>
   );
 }
