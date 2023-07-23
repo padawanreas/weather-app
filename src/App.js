3fa192df8078bb955db1b8e0cabc2c4c77bd6ab0
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-07-23 14:38:53 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { base_url, api_key } from "./components/Configs/Api";
+import { AiOutlineSearch } from "react-icons/ai";
 import "./App.css";
 import Weather from "./components/Weather/Weather";
 
 function App() {
-  const [weatherData, setWeatherData] = useState([]);
-  useEffect(() => {
-    const getWeather = async () => {
-      const response = await axios.get(
-        `${base_url}?lat=49.183333&lon=-0.350000&appid=${api_key}&lang=fr`
-      );
-      setWeatherData(response.data);
-    };
-    getWeather();
-  }, []);
+  const [weatherData, setWeatherData] = useState("");
+  const [location, setLocation] = useState("");
 
+  const url = `${base_url}?q=${location}&appid=${api_key}&units=metric&lang=fr`;
   //console.log(weatherData);
+  const searchLocation = async () => {
+    const response = await axios.get(url).then((response) => {
+      setWeatherData(response.data);
+      console.log(response.data);
+    });
+    setLocation("");
+  };
 
   return (
     <div className="App">
       <div className="header">
-        <p>WEATHER_APP</p>
         <div className="search">
-          <input placeholder="search location" type="text" />
+          <input
+            placeholder="search city"
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+
+          <AiOutlineSearch onClick={searchLocation} />
         </div>
       </div>
-      <Weather data={weatherData} />
+      <div className="container">
+        <Weather data={weatherData} />
+      </div>
     </div>
   );
 }
